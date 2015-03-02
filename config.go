4586df0c020f483789f3cf138c383e8601fb2443
Karl Matthias <kmatthias@newrelic.com>
2015-03-01 22:43:07 -0800

    Code to parse the config file.

---
+++ config.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"regexp"
+
+	"github.com/BurntSushi/toml"
+)
+
+type ServicesConfig struct {
+	NameMatch string `toml:"name_match"`
+	NameRegexp *regexp.Regexp
+}
+
+type Config struct {
+	Services ServicesConfig
+}
+
+func parseConfig(path string) Config {
+	var config Config
+
+	_, err := toml.DecodeFile(path, &config)
+	if err != nil {
+		exitWithError(err, "Failed to parse config file")
+	}
+
+	config.Services.NameRegexp, err = regexp.Compile(config.Services.NameMatch)
+	exitWithError(err, "Cant compile name_match regex")
+
+	return config
+}
