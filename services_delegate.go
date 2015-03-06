77d0d392e9eef0973ab41225a5fc6cd8cbb76ce2
Karl Matthias <kmatthias@newrelic.com>
2015-03-06 14:31:25 -0800

    Clarify log message.

--- services_delegate.go
+++ services_delegate.go
@@ -68,7 +68,7 @@ func (d *servicesDelegate) GetBroadcasts(overhead, limit int) [][]byte {
 	}
 
 	if broadcast == nil || len(broadcast) < 1 {
-		log.Println("Not enough space to fit any messages")
+		log.Println("Not enough space to fit any messages or message was nil")
 		return nil
 	}
 
