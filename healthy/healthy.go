205a5d0adfb3726894c386863b7e1d7cc21a3bb2
Karl Matthias <kmatthias@newrelic.com>
2015-03-12 17:22:48 -0700

    WIP health checking framework.

---
+++ healthy/healthy.go
@@ -0,0 +1,142 @@
+// A lightweight health-checking module so we can make
+// sure that services are running and healthy before
+// we announce them to our peers. Has a standard check
+// interval for all checks, not configurable per check.
+
+package healthy
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+const (
+	HEALTHY = 0
+	SICKLY  = iota
+	FAILED  = iota
+)
+
+const (
+	FOREVER = 0
+)
+
+type Monitor struct {
+	CheckInterval time.Duration
+	Checks []*Check
+	sync.RWMutex
+}
+
+type Check struct {
+	Status int
+	Count int
+	Type string
+	Args string
+	Command Checker
+}
+
+type Checker interface {
+	Run(args string) (int, error)
+}
+
+func NewCheck() *Check {
+	check := Check{
+		Count: 0,
+		Type: "http",
+		Command: &HttpCheck{},
+	}
+	return &check
+}
+
+func NewMonitor() *Monitor {
+	monitor := Monitor{
+		CheckInterval: 3 * time.Second,
+		Checks: make([]*Check, 0),
+	}
+	return &monitor
+}
+
+// Returns a list of all checks that are in a status other
+// than HEALTHY.
+func (m *Monitor) Unhealthy() []*Check {
+	var list []*Check
+	m.RLock()
+	defer m.RUnlock()
+
+	for _, check := range m.Checks {
+		if check.Status != HEALTHY {
+			list = append(list, check)
+		}
+	}
+	return list
+}
+
+// Returns a slice of checks that are in the HEALTHY status.
+func (m *Monitor) Healthy() []*Check {
+	var list []*Check
+	m.RLock()
+	defer m.RUnlock()
+
+	for _, check := range m.Checks {
+		if check.Status == HEALTHY {
+			list = append(list, check)
+		}
+	}
+	return list
+}
+
+func (m *Monitor) AddCheck(check *Check) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.Checks = append(m.Checks, check)
+}
+
+// Run the monitoring loop. Takes an argument of how many
+// times to run. -1 means to run forever.
+func (m *Monitor) Run(count int) {
+	c := time.Tick(m.CheckInterval)
+	i := 0
+	for range c {
+		log.Printf("Running checks")
+
+		var wg sync.WaitGroup
+
+		m.Lock()
+		defer m.Unlock()
+
+		wg.Add(len(m.Checks))
+log.Printf("Len: %d\n", len(m.Checks))
+		for _, check := range m.Checks {
+			// Run all checks in parallel in goroutines
+			go func(check *Check) {
+				// TODO add timeout around this call
+				result, err := check.Command.Run(check.Args)
+				log.Printf("res: %#v\n", result)
+				log.Printf("err: %#v\n", err)
+	log.Printf("%#v\n", check)
+				check.Status = result
+				wg.Done()
+			}(check) // copy check ptr for the goroutine
+		}
+
+		// Let's make sure we don't continue to spool up
+		// huge quantities of goroutines by waiting on all of them
+		// to complete before moving on. This could slow down
+		// our check loop if something doesn't time out properly.
+		wg.Wait()
+		// Don't increment in this case or we'll stop on maxint rollover
+		if count != - 1 {
+			i = i + 1
+			if i >= count {
+				return
+			}
+		}
+	}
+}
+
+type HttpCheck struct {}
+
+func (h *HttpCheck) Run(args string) (int, error) {
+	return HEALTHY, nil
+}
---
+++ healthy/healthy_test.go
@@ -0,0 +1,115 @@
+package healthy
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_NewCheck(t *testing.T) {
+	Convey("Returns a properly configured Check", t, func() {
+		check := NewCheck()
+
+		So(check.Count, ShouldEqual, 0)
+		So(check.Type, ShouldEqual, "http")
+		So(reflect.DeepEqual(check.Command, &HttpCheck{}), ShouldBeTrue)
+	})
+}
+
+func Test_NewMonitor(t *testing.T) {
+	Convey("Returns a properly configured Monitor", t, func() {
+		monitor := NewMonitor()
+
+		So(monitor.CheckInterval, ShouldEqual, 3 * time.Second)
+		So(len(monitor.Checks), ShouldEqual, 0)
+	})
+}
+
+func Test_Status(t *testing.T) {
+	Convey("Testing Status", t, func() {
+		monitor := NewMonitor()
+		monitor.Checks = []*Check{
+			&Check{Status: HEALTHY},
+			&Check{Status: HEALTHY},
+			&Check{Status: SICKLY},
+			&Check{Status: FAILED},
+		}
+
+		Convey("Healthy() returns a list of only healthy checks", func() {
+			list := monitor.Healthy()
+			So(len(list), ShouldEqual, 2)
+		})
+
+		Convey("Unhealthy() returns a list of anything but healthy checks", func() {
+			list := monitor.Unhealthy()
+			So(len(list), ShouldEqual, 2)
+		})
+	})
+}
+
+func Test_AddCheck(t *testing.T) {
+	Convey("Adds a check to the list", t, func() {
+		monitor := NewMonitor()
+		So(len(monitor.Checks), ShouldEqual, 0)
+		monitor.AddCheck(&Check{})
+		So(len(monitor.Checks), ShouldEqual, 1)
+		monitor.AddCheck(&Check{})
+		So(len(monitor.Checks), ShouldEqual, 2)
+	})
+}
+
+type mockCommand struct {
+	CallCount int
+	LastArgs string
+	DesiredResult int
+}
+
+func (m *mockCommand) Run(args string) (int, error) {
+	m.CallCount = m.CallCount + 1
+	m.LastArgs = args
+	return m.DesiredResult, nil
+}
+
+func Test_RunningChecks(t *testing.T) {
+	Convey("Working with health checks", t, func() {
+		monitor := NewMonitor()
+		monitor.CheckInterval = 1 * time.Nanosecond
+		cmd := mockCommand{DesiredResult: HEALTHY}
+		check := &Check{
+			Type: "mock",
+			Args: "testing",
+			Command: &cmd,
+		}
+		monitor.AddCheck(check)
+
+/*		Convey("The Check Command gets evaluated", func() {
+			monitor.Run(1)
+			So(cmd.CallCount, ShouldEqual, 1)
+			So(cmd.LastArgs, ShouldEqual, "testing")
+			So(cmd.DesiredResult, ShouldEqual, HEALTHY) // We know it's our cmd
+		})
+
+		Convey("Healthy Checks are marked healthy", func() {
+			monitor.Run(1)
+			So(cmd.CallCount, ShouldEqual, 1)
+			So(cmd.LastArgs, ShouldEqual, "testing")
+			So(check.Status, ShouldEqual, HEALTHY)
+		})
+*/
+		Convey("Unhealthy Checks are marked unhealthy", func() {
+			fail := mockCommand{DesiredResult: FAILED}
+			badCheck := &Check{
+				Type: "mock",
+				Args: "testing123",
+				Command: &fail,
+			}
+			monitor.Checks = []*Check{check, badCheck}
+			monitor.Run(1)
+
+			So(fail.CallCount, ShouldEqual, 1)
+			So(badCheck.Status, ShouldEqual, FAILED)
+		})
+	})
+}
