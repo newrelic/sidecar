40ed32ac4af930ad417403897ab22f7fccd1290a
Karl Matthias <karl.matthias@gonitro.com>
2015-12-23 20:05:43 -0800

    Working Docker image build.

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+die() {
+	echo $1
+	exit 1
+}
+
+file ../sidecar | grep "ELF.*LSB" || die "../sidecar is missing or not a Linux binary"
+cp ../sidecar . && cp -pr ../views . && docker build . || die "Failed to build"
--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,7 @@ for entry in $SIDECAR_SEEDS; do
 	CLI="$CLI --cluster-ip $entry"
 done
 
-if [[ -n $SIDECAR_ADVERTISE_IP ]]; then
+if [[ -n "$SIDECAR_ADVERTISE_IP" ]]; then
 	CLI="$CLI --advertise-ip $SIDECAR_ADVERTISE_IP"
 fi
 
