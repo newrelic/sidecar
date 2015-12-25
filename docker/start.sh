22f2a26deaac2afc7bee5944e1d95e573fcc4df6
Karl Matthias <karl.matthias@gonitro.com>
2015-12-24 18:03:39 -0800

    Support adding an IP address on startup.
    This is used when bind_adress for HAproxy is not an
    existing address.

--- docker/start.sh
+++ docker/start.sh
@@ -9,4 +9,13 @@ if [[ -n "$SIDECAR_ADVERTISE_IP" ]]; then
 	CLI="$CLI --advertise-ip $SIDECAR_ADVERTISE_IP"
 fi
 
+BIND_IP=`grep bind_ip sidecar.toml | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*"`
+
+# If there's a BIND_IP and we don't already have it, add the
+# address to the loopback interface.
+if [[ -n $BIND_IP ]] && [[ $BIND_IP != "0.0.0.0" ]] || [[ ! ip addr show | grep $BIND_IP ]]; then
+	echo "Adding $BIND_IP to the loopback interface"
+	ip addr add $BIND_IP/32 dev lo
+fi
+
 ./sidecar $CLI
