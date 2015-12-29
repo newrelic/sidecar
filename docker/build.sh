7c44277d4605e4bc1bbf360b04b7e46c1c3e2763
Karl Matthias <karl.matthias@gonitro.com>
2015-12-29 09:32:52 -0800

    Make sure we actually get the sidecar.toml.

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,5 @@ die() {
 }
 
 file ../sidecar | grep "ELF.*LSB" || die "../sidecar is missing or not a Linux binary"
+cp sidecar.docker.toml sidecar.toml
 cp ../sidecar . && cp -pr ../views . && docker build . || die "Failed to build"
