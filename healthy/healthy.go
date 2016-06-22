0b140fb3b712ef337d1b82d329892575ff7d2438
Karl Matthias <karl.matthias@gonitro.com>
2016-06-22 13:44:25 +0100

    Log the check ID with the error about executing it.

--- healthy/healthy.go
+++ healthy/healthy.go
@@ -95,7 +95,7 @@ func NewCheck(id string) *Check {
 // of the current Check.
 func (check *Check) UpdateStatus(status int, err error) {
 	if err != nil {
-		log.Errorf("Error executing check, status UNKNOWN")
+		log.Warnf("Error executing check, status UNKNOWN: (id %s)", check.ID)
 		check.Status = UNKNOWN
 		check.LastError = err
 	} else {
