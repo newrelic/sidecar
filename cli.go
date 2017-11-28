1b139224e18014a614a62ca7c960e7b6eac34326
Karl Matthias <karl.matthias@gonitro.com>
2017-11-28 10:13:22 +0000

    Remove references to config file

--- cli.go
+++ cli.go
@@ -10,7 +10,6 @@ import (
 type CliOpts struct {
 	AdvertiseIP    *string
 	ClusterIPs     *[]string
-	ConfigFile     *string
 	ClusterName    *string
 	CpuProfile     *bool
 	Discover       *[]string
@@ -31,7 +30,6 @@ func parseCommandLine() *CliOpts {
 
 	opts.AdvertiseIP = app.Flag("advertise-ip", "The address to advertise to the cluster").Short('a').String()
 	opts.ClusterIPs = app.Flag("cluster-ip", "The cluster seed addresses").Required().Short('c').NoEnvar().Strings()
-	opts.ConfigFile = app.Flag("config-file", "The config file to use").Short('f').Default("sidecar.toml").String()
 	opts.ClusterName = app.Flag("cluster-name", "The cluster we're part of").Short('n').Default("default").String()
 	opts.CpuProfile = app.Flag("cpuprofile", "Enable CPU profiling").Short('p').Bool()
 	opts.Discover = app.Flag("discover", "Method of discovery").Short('d').NoEnvar().Strings()
