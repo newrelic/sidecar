newrelic/sidecar

commit 275b89b752708e81b7c6f092a92739012f444d31

Make sure we tombstone orphan services from dead hosts, no matter what state they're in

catalog/services_state.go
@@ -604,27 +604,27 @@ func (state *ServicesState) TombstoneOthersServices() []service.Service {
 			if len(state.Servers[*hostname].Services) < 1 {
 				delete(state.Servers, *hostname)
 			}
-		}
+		} else {
+			// Everything that is not tombstoned needs to be considered for
+			// removal if it exceeds the allowed ALIVE_TIMESPAN
+			if svc.Updated.Before(time.Now().UTC().Add(0 - ALIVE_LIFESPAN)) {
+				log.Warnf("Found expired service %s from %s, tombstoning",
+					svc.Name, svc.Hostname,
+				)
 
-		if svc.IsAlive() &&
-			svc.Updated.Before(time.Now().UTC().Add(0-ALIVE_LIFESPAN)) {
+				// Because we don't know that other hosts haven't gotten a newer
+				// message that we missed, we'll tombstone them with the original
+				// timestamp + 1 second. This way we don't invalidate newer records
+				// we didn't see. This might happen when any node is removed from
+				// cluster and re-joins, for example. So we can't use svc.Tombstone()
+				// which updates the timestamp to Now().UTC()
+				previousStatus := svc.Status
+				svc.Status = service.TOMBSTONE
+				svc.Updated = svc.Updated.Add(time.Second)
+				state.ServiceChanged(svc, previousStatus, svc.Updated)
 
-			log.Warnf("Found expired service %s from %s, tombstoning",
-				svc.Name, svc.Hostname,
-			)
-
-			// Because we don't know that other hosts haven't gotten a newer
-			// message that we missed, we'll tombstone them with the original
-			// timestamp + 1 second. This way we don't invalidate newer records
-			// we didn't see. This might happen when any node is removed from
-			// cluster and re-joins, for example. So we can't use svc.Tombstone()
-			// which updates the timestamp to Now().UTC()
-			previousStatus := svc.Status
-			svc.Status = service.TOMBSTONE
-			svc.Updated = svc.Updated.Add(time.Second)
-			state.ServiceChanged(svc, previousStatus, svc.Updated)
-
-			result = append(result, *svc)
+				result = append(result, *svc)
+			}
 		}
 	})
 

commit 341410ec0138fed128b9e1fe8dc5518526a959c4

Avoid tombstoning tombstones
(bug introduced by 275b89b)

catalog/services_state.go
@@ -604,27 +604,28 @@ func (state *ServicesState) TombstoneOthersServices() []service.Service {
 			if len(state.Servers[*hostname].Services) < 1 {
 				delete(state.Servers, *hostname)
 			}
-		} else {
-			// Everything that is not tombstoned needs to be considered for
-			// removal if it exceeds the allowed ALIVE_TIMESPAN
-			if svc.Updated.Before(time.Now().UTC().Add(0 - ALIVE_LIFESPAN)) {
-				log.Warnf("Found expired service %s from %s, tombstoning",
-					svc.Name, svc.Hostname,
-				)
+		}
 
-				// Because we don't know that other hosts haven't gotten a newer
-				// message that we missed, we'll tombstone them with the original
-				// timestamp + 1 second. This way we don't invalidate newer records
-				// we didn't see. This might happen when any node is removed from
-				// cluster and re-joins, for example. So we can't use svc.Tombstone()
-				// which updates the timestamp to Now().UTC()
-				previousStatus := svc.Status
-				svc.Status = service.TOMBSTONE
-				svc.Updated = svc.Updated.Add(time.Second)
-				state.ServiceChanged(svc, previousStatus, svc.Updated)
+		// Everything that is not tombstoned needs to be considered for
+		// removal if it exceeds the allowed ALIVE_TIMESPAN
+		if !svc.IsTombstone() &&
+			svc.Updated.Before(time.Now().UTC().Add(0-ALIVE_LIFESPAN)) {
+			log.Warnf("Found expired service %s from %s, tombstoning",
+				svc.Name, svc.Hostname,
+			)
 
-				result = append(result, *svc)
-			}
+			// Because we don't know that other hosts haven't gotten a newer
+			// message that we missed, we'll tombstone them with the original
+			// timestamp + 1 second. This way we don't invalidate newer records
+			// we didn't see. This might happen when any node is removed from
+			// cluster and re-joins, for example. So we can't use svc.Tombstone()
+			// which updates the timestamp to Now().UTC()
+			previousStatus := svc.Status
+			svc.Status = service.TOMBSTONE
+			svc.Updated = svc.Updated.Add(time.Second)
+			state.ServiceChanged(svc, previousStatus, svc.Updated)
+
+			result = append(result, *svc)
 		}
 	})
 

catalog/services_state_test.go
@@ -431,6 +431,15 @@ func Test_TrackingAndBroadcasting(t *testing.T) {
 			So(svcs["unknown_shakespeare"].Status, ShouldEqual, service.TOMBSTONE)
 		})
 
+		Convey("Tombstones aren't re-tombstoned", func() {
+			tombstonedService := service.Service{ID: "dead_shakespeare", Hostname: hostname, Updated: baseTime, Status: service.TOMBSTONE}
+			state.AddServiceEntry(tombstonedService)
+
+			svcs := state.TombstoneOthersServices()
+
+			So(svcs, ShouldBeNil)
+		})
+
 		Convey("Can detect new services or newly changed services", func() {
 			// service1 and services[0] are copies of the same service
 			service1.Status = service.UNHEALTHY